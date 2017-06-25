60f0df73263315c163adab377023e6ddeb8a197f
Joseph Lin <Joseph Lin>
2017-06-24 20:08:24 -0700

    add

---
+++ server.js
@@ -0,0 +1,18 @@
+// REQUIRE
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// EXPRESS
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// SET UP EXPRESS 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+})
