c2c2fa4fc44f4e28a6527b87f85193f4f5fa11d5
Joseph Lin <Joseph Lin>
2017-07-02 14:25:23 -0700

    add

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // REQUIRE
 var express = require("express");
 var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 3000;
 
 // EXPRESS
 var app = express();
@@ -11,8 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-
-app.listen(PORT, function(){
-    console.log("App listening on PORT " + PORT);
-})
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+var routes = require("./controllers/burgersController.js");
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
+app.listen(PORT, function() {
+  console.log("Listening on port:%s", PORT);
+});
